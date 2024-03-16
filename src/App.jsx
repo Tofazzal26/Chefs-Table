4c45d4dd6a9ba869efee6f009df57f08b2120485
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-16 20:06:21 +0600

    Preparing style finish

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,42 @@ import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
 import OurRecipe from "./Components/OurRecipe/OurRecipe";
 import Recipes from "./Components/Recipes/Recipes";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   const [recipes, setRecipes] = useState([]);
 
   const handleCook = (recipe) => {
     const newRecipe = [...recipes, recipe];
-    setRecipes(newRecipe);
+
+    const isExisted = recipes.find((idx) => idx.id == recipe.id);
+
+    if (!isExisted) {
+      setRecipes(newRecipe);
+    } else {
+      toast("Already Existed !");
+    }
   };
 
   return (
     <>
-      <div className="lexEnd container mx-auto">
-        <div>
-          <Header />
-          <Main />
-          <OurRecipe />
-          <div className="grid grid-cols-1 lg:grid-cols-6 gap-6">
-            <div className="col-span-4">
-              <Recipes handleCook={handleCook} />
-            </div>
-            <div className="col-span-2">
-              <Cooking recipes={recipes} />
+      <div>
+        <div className="lexEnd container mx-auto">
+          <div>
+            <Header />
+            <Main />
+            <OurRecipe />
+            <div className="grid grid-cols-1 lg:grid-cols-6 gap-6">
+              <div className="col-span-4">
+                <Recipes handleCook={handleCook} />
+              </div>
+              <div className="col-span-2">
+                <Cooking recipes={recipes} />
+              </div>
             </div>
           </div>
         </div>
+        <ToastContainer />
       </div>
     </>
   );
--- src/Components/Cooking/Cooking.jsx
+++ src/Components/Cooking/Cooking.jsx
@@ -2,7 +2,7 @@ const Cooking = ({ recipes }) => {
   return (
     <div className="border-2 rounded-md">
       <h2 className="text-2xl font-semibold text-center mt-6">
-        Want to cook: 01
+        Want to cook: {recipes.length}
       </h2>
       <div className="divider px-16"></div>
       <div>
@@ -15,20 +15,54 @@ const Cooking = ({ recipes }) => {
                 <th>Name</th>
                 <th>Time</th>
                 <th>Calories</th>
+                <th>Preparing</th>
               </tr>
             </thead>
             <tbody>
               {/* row 1 */}
               {recipes.map((recipe, ind) => (
-                <tr key={ind} className="text-[14px]">
+                <tr key={ind} className="text-[14px] bg-[#f8f8f8]">
                   <th>{ind + 1}</th>
                   <td>{recipe.name}</td>
                   <td>{recipe.time} minutes</td>
                   <td>{recipe.Calories} Calories</td>
+                  <td>
+                    <button className="py-2 px-4 bg-[#0be58a] text-[#150B2B] font-semibold hover:bg-[#f4bf2c] rounded-full">
+                      Preparing
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
+        </div>
+        <div>
+          <h2 className="text-2xl font-semibold text-center mt-4 mb-4">
+            Currently cooking: 02
+          </h2>
+          <div className="overflow-x-auto">
+            <table className="table text-[#878787]">
+              {/* head */}
+              <thead>
+                <tr className="text-[14px]">
+                  <th></th>
+                  <th>Name</th>
+                  <th>Time</th>
+                  <th>Calories</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+
+                <tr className="text-[14px] bg-[#f8f8f8]">
+                  <th></th>
+                  <td>hello</td>
+                  <td> minutes</td>
+                  <td> Calories</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
